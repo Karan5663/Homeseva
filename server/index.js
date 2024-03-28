5f24cfdb9e2786e54f8d66c4760b800eb0b33cbe
Karan5663 <132004682+Karan5663@users.noreply.github.com>
2024-03-28 15:04:05 +0530

    commit 18
    mongodb connectivity

--- server/index.js
+++ server/index.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose =require('mongoose');
 const bodyparser = require('body-parser');
 const server = express();
+
+main().catch(err=>console.log(err));
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/HomeSeva');
+    console.log('db connected')
+}
+
+const HomeSeva = new mongoose.Schema({
+    username: String,
+    Email:String,
+    Password: String,
+    phone: String
+  });
+
+const User = mongoose.model('user', HomeSeva);  
 
 server.use(cors());
 server.use(bodyparser.json());
 
-server.post('/demo',(req,res)=>{
+server.post('/demo',async (req,res)=>{
+    let user = new User();
+    user.username = req.body.username;
+    user.Email= req.body.Email;
+    user.Password = req.body.Password;
+    user.phone= req.body.phone;
+    
+    
+    const doc = await user.save();
     console.log(req.body)
     res.json(req.body);
 })
